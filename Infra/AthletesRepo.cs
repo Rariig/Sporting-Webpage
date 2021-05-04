0cb5c3881e1fb5e3f6462eb8d0bbf4cf81af40bc
Raoul Rocco Riigov <rariig@ttu.ee>
2021-05-04 04:19:21 +0300

    Paar vigast muudatust, kommentisin välja, aga hiljem läheb neid vaja.

--- Infra/AthletesRepo.cs
+++ Infra/AthletesRepo.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
+using Domain.Repos;
+using Infra.Common;
+using SportEU.Domain;
 
 namespace Infra
 {
-    class AthletesRepo
+    /* public sealed class AthletesRepo : PagedRepo<Athlete, AthleteData>, IAthletesRepo
     {
-    }
+    } */
 }
--- Pages/AthletesModel.cs
+++ Pages/AthletesModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Facade;
+using Pages.Common;
+using SportEU.Domain;
+using SportEU.Infra;
 
 namespace Pages
 {
-    class AthletesModel
+   /* class AthletesModel : BasePage<Athlete,AthleteView>
     {
-    }
+        public override string PageTitle => "Athletes";
+
+        public AthletesModel(ApplicationDbContext c) : this(new AthletesRepo(c), c) { }
+    } */
 }
