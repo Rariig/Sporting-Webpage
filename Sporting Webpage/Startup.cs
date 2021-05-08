c4822f88c8f3a22176d1a831885e44b12007ed89
Raoul Rocco Riigov <rariig@ttu.ee>
2021-05-08 04:06:02 +0300

    Registreerisin repod startupis. Soft/Athlete/Index ei toimi veel korralikult.

--- Sporting Webpage/Startup.cs
+++ Sporting Webpage/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportEU.Domain.Repos;
 using SportEU.Infra;
 
 namespace SportEU
@@ -22,13 +23,17 @@ namespace SportEU
 
        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
+            services.AddTransient<IAthletesRepo, AthletesRepo>();
+            services.AddTransient<ICoachesRepo, CoachesRepo>();
+            services.AddTransient<IGroupsRepo, GroupsRepo>();
+            services.AddTransient<IGroupAssignmentsRepo, GroupAssignmentsRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
